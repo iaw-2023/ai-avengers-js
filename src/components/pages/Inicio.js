1423d9ce8685c55cf4195392b90ae7b2b2186b62
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2024-02-23 13:10:52 -0300

    Update Inicio.js

--- src/components/pages/Inicio.js
+++ src/components/pages/Inicio.js
@@ -91,7 +91,7 @@ const Inicio = () => {
       {vehiculos && vehiculos.length > 0 ? (
       <div className={isCardView ? "card-container-shopping" : "list-container-shopping"}>
           {vehiculos.map((vehiculo) => (
-            <div className="card text-bg-primary mb-3" style={{ maxWidth: "18rem" }} key={vehiculo.id} >
+            <div style={{ maxWidth: "18rem" }} key={vehiculo.id} >
               {isCardView ? (
                 <ProductItem
                   vehiculo={vehiculo}
