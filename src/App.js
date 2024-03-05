5d19849fcb76f7f3bb1ce02d6bc681d768e5874b
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2024-03-05 15:55:06 -0300

    Login y Logout
    Funcionan ambos, ademas hay una pestaña de profile para sumar algun detalle.
    Faltaria hacerlo todo mas lindo.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
 import Navbar from './components/navegation/Navbar'
 import Inicio from './components/pages/Inicio'
 import Shoppingcart from './components/pages/Shoppingcart'
 import Help from './components/pages/Help'
 import Error from './components/pages/Error'
+import LoginButton from './components/Login'
+import LogoutButton from './components/Logout'
+import Profile from './components/Profile'
 
 const App = () =>  {
-  return (
+  const {isAuthenticated} = useAuth0();
+  
+  return ( 
     <div className="App">
-      <Router>
+      {isAuthenticated ? (
+        <>
+        <Profile /> 
+        <LogoutButton/>
+        <Router>
         <Navbar/>
         <Routes>
-          <Route path='/' exact Component={Inicio}/>
-          <Route path='/Shoppingcart' Component={Shoppingcart}/>
-          <Route path='/Help' Component={Help}/>
-          <Route path='/Error' Component={Error}/> 
-        </Routes>
-      </Router>
+            <Route path='/' exact Component={Inicio}/>
+            <Route path='/Shoppingcart' Component={Shoppingcart}/>
+            <Route path='/Help' Component={Help}/>
+            <Route path='/Error' Component={Error}/> 
+          </Routes>
+        </Router>
+        </>
+      ) : (
+        <LoginButton />
+      )}
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-//import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  /* document.getElementById('root') */
+const domain = 'dev-7jdip8215elmq7hg.us.auth0.com'; // Reemplaza con tu dominio de Auth0
+const clientId = '0wWvG46XWAb7G1pyhZL2U3XT24AqUeM3';   // Reemplaza con tu ID de cliente
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Auth0Provider
+        domain={domain}
+        clientId={clientId}
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
+        <App />
+      </Auth0Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-//serviceWorkerRegistration.register();
-
-//reportWebVitals();
