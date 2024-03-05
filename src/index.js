ec9dfcfd9b53eeb0d5aa159a976e0c774f801f3f
Ezequiel Zucoli <ezequielzucoli@hotmail.com>
2024-03-05 17:29:29 -0300

    Fix render index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
@@ -7,7 +7,8 @@ import App from './App';
 const domain = 'dev-7jdip8215elmq7hg.us.auth0.com'; // Reemplaza con tu dominio de Auth0
 const clientId = '0wWvG46XWAb7G1pyhZL2U3XT24AqUeM3';   // Reemplaza con tu ID de cliente
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <React.StrictMode>
       <Auth0Provider
         domain={domain}
@@ -17,5 +18,4 @@ ReactDOM.render(
         <App />
       </Auth0Provider>
     </React.StrictMode>,
-    document.getElementById('root')
 );
